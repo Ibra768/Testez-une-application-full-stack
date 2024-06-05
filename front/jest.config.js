606fd73780143634c41b15c25cdf9be9f31ed0dc
Ibrahim <i.benfaiala@gmail.com>
2024-06-05 20:40:37 +0200

    fix jest config

--- front/jest.config.js
+++ front/jest.config.js
@@ -7,12 +7,6 @@ module.exports = {
   bail: false,
   verbose: false,
   collectCoverage: true,
-  collectCoverageFrom: [
-    "src/**/*.ts", // Include .ts files
-    "!src/**/*.spec.ts", // Exclude .spec.ts files
-    "!src/**/*.html", // Exclude .html files
-  ],
-  coverageReporters: ['lcov', 'text'],
   coverageDirectory: './coverage/jest',
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],

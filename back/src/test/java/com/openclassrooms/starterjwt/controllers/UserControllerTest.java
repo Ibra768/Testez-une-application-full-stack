36a4fdb8c356a79612fcbd20b9c98b73d2f9ad68
BENFAIALA Ibrahim <i.benfaiala@gmail.com>
2024-06-25 13:15:31 +0200

    rework teacher and user controller tests

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/UserControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/UserControllerTest.java
@@ -1,26 +1,38 @@
 package com.openclassrooms.starterjwt.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.dto.TeacherDto;
 import com.openclassrooms.starterjwt.dto.UserDto;
 import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.payload.request.LoginRequest;
+import com.openclassrooms.starterjwt.payload.response.JwtResponse;
 import com.openclassrooms.starterjwt.repository.UserRepository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
@@ -29,15 +41,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@DirtiesContext
 @AutoConfigureMockMvc
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:script.sql")
 public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    UserRepository $userRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Nested
     public class FindById {
@@ -46,118 +64,71 @@ public class UserControllerTest {
         @WithMockUser
         void shouldHaveId() throws Exception {
 
-            UserDto userDto = new UserDto(1L,"example@mail.com","Doe","John",true,"Password",null,null);
-            User user = new User(1L,"example@mail.com","Doe","John","Password",true,null,null);
-            // GIVEN
-            when($userRepository.findById(anyLong())).thenReturn(Optional.of(user));
-
-            // WHEN & THEN
             mockMvc.perform(get("/api/user/1")
-                            .contentType(MediaType.APPLICATION_JSON))
+                            .accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
-                    .andExpect(content().json(new ObjectMapper().writeValueAsString(userDto)));
-
-            verify($userRepository,times(1)).findById(anyLong());
+                    .andDo(r -> {
+                        String result = r.getResponse().getContentAsString();
+                        TeacherDto teacher = objectMapper.readValue(result, TeacherDto.class);
+                        assertNotNull(teacher);
+                        assertEquals("Admin", teacher.getFirstName());
+                    });
         }
 
         @Test
         @WithMockUser
         void shouldNotFound() throws Exception {
-
-            // GIVEN
-            when($userRepository.findById(anyLong())).thenReturn(Optional.empty());
-
-            // WHEN & THEN
-            mockMvc.perform(get("/api/user/1")
-                            .contentType(MediaType.APPLICATION_JSON))
+            mockMvc.perform(get("/api/user/99")
+                    .contentType(MediaType.APPLICATION_JSON))
                     .andExpect(status().isNotFound());
-
-            verify($userRepository, times(1)).findById(anyLong());
         }
         @Test
         @WithMockUser
         void shouldBadRequest() throws Exception {
-
-            // WHEN & THEN
             mockMvc.perform(get("/api/user/not_a_number")
-                            .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isBadRequest());
-
-            verify($userRepository, times(0)).findById(anyLong());
-
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
         }
     }
 
     @Nested
     public class Save{
+
+        @BeforeEach
+        void setUp() {
+            LoginRequest loginRequest = new LoginRequest();
+            loginRequest.setEmail("gym@studio.com");
+            loginRequest.setPassword("Mypassword8$");
+            restTemplate.postForEntity("/api/auth/login", loginRequest, JwtResponse.class);
+        }
+
         @Test
-        @WithMockUser(username = "example@mail.com", roles = {"USER"})
-        void shouldSave() throws Exception {
-
-            User user = new User(1L,"example@mail.com","Doe","John","Password",true,null,null);
-            // GIVEN
-            when($userRepository.findById(anyLong())).thenReturn(Optional.of(user));
-
-            // WHEN & THEN
-            mockMvc.perform(delete("/api/user/1")
-                            .contentType(MediaType.APPLICATION_JSON))
+        void shouldDelete() throws Exception {
+            mockMvc.perform(delete("/api/user/2")
+                            .with(SecurityMockMvcRequestPostProcessors.user("gym@studio.com"))
+                            .with(SecurityMockMvcRequestPostProcessors.csrf()))
                     .andExpect(status().isOk());
-
-            verify($userRepository,times(1)).findById(anyLong());
-            verify($userRepository,times(1)).deleteById(anyLong());
         }
 
         @Test
-        @WithMockUser
         void shouldNotFound() throws Exception {
-
-            // GIVEN
-            when($userRepository.findById(anyLong())).thenReturn(Optional.empty());
-
-            // WHEN & THEN
-            mockMvc.perform(delete("/api/user/1")
-                            .contentType(MediaType.APPLICATION_JSON))
+            mockMvc.perform(delete("/api/user/99")
+                            .with(SecurityMockMvcRequestPostProcessors.user("gym@studio.com"))
+                            .with(SecurityMockMvcRequestPostProcessors.csrf()))
                     .andExpect(status().isNotFound());
-
-            verify($userRepository,times(1)).findById(anyLong());
-            verify($userRepository,times(0)).deleteById(anyLong());
         }
         @Test
-        @WithMockUser
         void shouldUnauthorized() throws Exception {
-
-            User user = new User(1L,"example@mail.com","Doe","John","Password",true,null,null);
-            // GIVEN
-            when($userRepository.findById(anyLong())).thenReturn(Optional.of(user));
-
-            // Arrange
-            UserDetails mockUserDetails = org.springframework.security.core.userdetails.User.withUsername("example@mail.com").password("password").roles("USER").build();
-            Authentication mockAuthentication = mock(Authentication.class);
-            SecurityContext mockSecurityContext = mock(SecurityContext.class);
-
-            when(mockAuthentication.getPrincipal()).thenReturn(mockUserDetails);
-            when(mockSecurityContext.getAuthentication()).thenReturn(mockAuthentication);
-
-            SecurityContextHolder.setContext(mockSecurityContext);
-
-            // WHEN & THEN
             mockMvc.perform(delete("/api/user/1")
-                            .contentType(MediaType.APPLICATION_JSON))
+                            .with(SecurityMockMvcRequestPostProcessors.user("gym@studio.com"))
+                            .with(SecurityMockMvcRequestPostProcessors.csrf()))
                     .andExpect(status().isUnauthorized());
-
-            verify($userRepository,times(1)).findById(anyLong());
-            verify($userRepository,times(0)).deleteById(anyLong());
-
         }
         void shouldBadRequest() throws Exception {
-
-            // WHEN & THEN
-            mockMvc.perform(delete("/api/user/not_a_number")
-                            .contentType(MediaType.APPLICATION_JSON))
+            mockMvc.perform(delete("/api/user/2")
+                            .with(SecurityMockMvcRequestPostProcessors.user("gym@studio.com"))
+                            .with(SecurityMockMvcRequestPostProcessors.csrf()))
                     .andExpect(status().isBadRequest());
-
-            verify($userRepository, times(0)).findById(anyLong());
-
         }
     }
 

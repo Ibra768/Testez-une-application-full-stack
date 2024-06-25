cece87be35885b0ec61c04c908ae94e7e095d9a8
BENFAIALA Ibrahim <i.benfaiala@gmail.com>
2024-06-25 15:53:25 +0200

    h2 database added

--- back/src/test/resources/script.sql
+++ back/src/test/resources/script.sql
@@ -1,13 +1,44 @@
+DROP TABLE IF EXISTS `PARTICIPATE`;
+DROP TABLE IF EXISTS `SESSIONS`;
+DROP TABLE IF EXISTS `TEACHERS`;
+DROP TABLE IF EXISTS `USERS`;
+CREATE TABLE `TEACHERS` (
+                            `id` INT PRIMARY KEY AUTO_INCREMENT,
+                            `last_name` VARCHAR(40),
+                            `first_name` VARCHAR(40),
+                            `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
-DELETE FROM PARTICIPATE;
-DELETE FROM SESSIONS;
-DELETE FROM TEACHERS;
-DELETE FROM USERS;
+CREATE TABLE `SESSIONS` (
+                            `id` INT PRIMARY KEY AUTO_INCREMENT,
+                            `name` VARCHAR(50),
+                            `description` VARCHAR(2000),
+                            `date` TIMESTAMP,
+                            `teacher_id` int,
+                            `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
-ALTER TABLE PARTICIPATE AUTO_INCREMENT = 1;
-ALTER TABLE SESSIONS AUTO_INCREMENT = 1;
-ALTER TABLE TEACHERS AUTO_INCREMENT = 1;
-ALTER TABLE USERS AUTO_INCREMENT = 1;
+CREATE TABLE `USERS` (
+                         `id` INT PRIMARY KEY AUTO_INCREMENT,
+                         `last_name` VARCHAR(40),
+                         `first_name` VARCHAR(40),
+                         `admin` BOOLEAN NOT NULL DEFAULT false,
+                         `email` VARCHAR(255),
+                         `password` VARCHAR(255),
+                         `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                         `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `PARTICIPATE` (
+                               `user_id` INT,
+                               `session_id` INT
+);
+
+ALTER TABLE `SESSIONS` ADD FOREIGN KEY (`teacher_id`) REFERENCES `TEACHERS` (`id`);
+ALTER TABLE `PARTICIPATE` ADD FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`);
+ALTER TABLE `PARTICIPATE` ADD FOREIGN KEY (`session_id`) REFERENCES `SESSIONS` (`id`);
 
 
 INSERT INTO TEACHERS (first_name, last_name)
